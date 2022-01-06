e7a879c5d135f2c60a66bb71a8e481e4dd34c936
kadzima <adz13004@byui.edu>
2022-01-06 08:40:27 -0600

    initial commit

---
+++ home.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en-us">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<title>Template</title>
+<meta name="description" content="Build Template">
+  
+<link href="\css\small.css" rel="stylesheet">
+<link href="\css\large.css" rel="stylesheet">
+<img src="\phpmotors\images\site\logo.png" alt="My Logo" width="250" height="100">
+</head>
+
+<body> 
+<span>
+<button class="ham" id="hamburger">☰</button>
+</span>  
+<nav>
+<ul class="navigation">
+<li><a href="/home.php" class="active">Home</a></li>
+<li><a href="/classic.php">Classic</a></li>
+<li><a href="/suv.php">SUV</a></li>
+<li><a href="/sports.php">Sports</a></li>
+<li><a href="/truck.php">Trucks</a></li>
+<li><a href="/used.php">Used</a></li>
+</ul>
+</nav>
+<main>
+<header>
+<h1>Welcome to PHP Motor!</h1>
+</header>
+<section>
+<p>DMC Delorean</p>
+<br>
+<p>3 Cups holders</p>
+<p>Superman doors</p>
+<p>Fuzzy dice</p>
+</section>
+<img src="\phpmotors\images\site\own_today.png" alt="Qwn_today">
+<img src="\phpmotors\images\delorean.jpg" alt="delorean">
+ 
+<h2>Upgrades</h2>
+<div class="upgrades">
+<figure>
+<img src="\phpmotors\images\upgrades\bumper_sticker.jpg" alt="Bumper">
+</figure>
+<figure>
+<img src="\phpmotors\images\upgrades\flame.jpg" alt="Flame" data-src="phpmotors\images\upgrades\flame.jpg">
+<figure>
+<img src="\phpmotors\images\upgrades\flux-cap.png" alt="Flux-cap" data-src="phpmotors\images\upgrades\flux-cap.png">
+</figure>
+<figure>
+<img src="\phpmotors\images\upgrades\hub-cap.jpg" alt="Hub-cap" data-src="phpmotors\images\upgrades\hub-cap.jpg">
+</figure>
+
+
+</main>
+
+
+
+
+<footer>
+<p> &copy;|Kelly Adzima| Accra-Ghana| lastupdate | <a href="https://www.byui.edu/online">BYUI Online Learning</a>|<span id="currentdate"></span></p>
+    
+</footer>
+    
+</body>
+</html>  
